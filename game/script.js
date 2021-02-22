d29a9f9c04f435e5358e86f9226a0ed524f4caa4
AppleVegas <40367739+AppleVegas@users.noreply.github.com>
2021-02-22 13:07:23 +0300

    Added heal

--- game/script.js
+++ game/script.js
@@ -1,11 +1,13 @@
 const canvas = document.getElementById('thegame')
-var mousex
-var mousey
+var mousex = 0
+var mousey = 0
 var w = $(window).width();
 var h = $(window).height();
 var oldw = w;
 var oldh = h;
 var mode = 1
+
+var istouchdevice = false;
 
 if (w < h){
     mode = 0
@@ -16,6 +18,7 @@ var shootsound = new Audio('shoot.wav');
 var hitsound = new Audio('hit.wav');
 var startsound = new Audio('start.wav');
 var dmgsound = new Audio('dmg.wav');
+var healsound = new Audio('heal.wav');
 
 function ScreenScale(int)
 {
@@ -48,6 +51,10 @@ function changeResolution(cnvs, width, height)
     cnvs.style.height = height.toString() + "px"
     $(cnvs).attr("width", width.toString() + "px");
     $(cnvs).attr("height", height.toString() + "px");
+}
+
+function clamp(num, min, max) {
+    return num <= min ? min : num >= max ? max : num;
 }
 
 changeResolution(canvas, w, h)
@@ -133,7 +140,8 @@ const TheGame = {
         SpawnDelay: 2, 
         Started: false,
         GameOver: false,
-        HurtRed: 0
+        HurtRed: 0,
+        HealGreen: 0
     },
     Player: {
         RotationTime: 0.5,
@@ -167,7 +175,7 @@ TheGame.FireBullet = function(x,y,angle, speed = 1){
 }
 
 
-TheGame.SpawnMeteor = function(x,y, size = 10, speed = 0.5){
+TheGame.SpawnMeteor = function(x,y, size = 10, speed = 0.5, healing, twox){
     let SingleMeteor = {
         Speed: speed,
         X: x,
@@ -175,7 +183,9 @@ TheGame.SpawnMeteor = function(x,y, size = 10, speed = 0.5){
         Ang: Math.atan2(h/2 - y, w/2 - x)* 180 / Math.PI,
         Size: size,
         SpawnW: w,
-        SpawnH: h
+        SpawnH: h,
+        IsHealing: healing,
+        IsTwoX: twox
     }
     TheGame.Meteors.push(SingleMeteor)
 }
@@ -208,9 +218,9 @@ function handlepress(){
 //$( "body" ).click(function() {
 //    handlepress()
 //});
-
 canvas.addEventListener('touchstart', function(e) {
-    let touch = e.originalEvent.touches[0];
+    istouchdevice = true
+    let touch = e.touches[0];
     mousex = touch.pageX;
     mousey = touch.pageY;
 
@@ -218,11 +228,15 @@ canvas.addEventListener('touchstart', function(e) {
 })
 
 canvas.addEventListener('mousedown', function(e) {
-    mousex = e.clientX;
-    mousey = e.clientY;
+    if (!istouchdevice)
+    {
+        mousex = e.clientX;
+        mousey = e.clientY;
 
-    handlepress()
+        handlepress()
+    }
 })
+
 
 function BulletMove(){
     for (let i = 0; i < TheGame.Bullets.length; i++) {
@@ -259,6 +273,7 @@ function MeteorMove(){
     for (let i = 0; i < TheGame.Meteors.length; i++) {
         let scale = Scale(TheGame.Meteors[i].Size)
         let scaletopow = scale*scale
+        let sizebyfour = TheGame.Meteors[i].Size/4
         if (TheGame.Meteors[i].SpawnW != w)
         {
             TheGame.Meteors[i].X = TheGame.Meteors[i].X * (w / TheGame.Meteors[i].SpawnW)
@@ -276,7 +291,7 @@ function MeteorMove(){
         {
             
             TheGame.Constants.HurtRed = 150
-            let resulthp = TheGame.Player.Health - Math.round(TheGame.Meteors[i].Size/4)
+            let resulthp = TheGame.Player.Health - Math.round(sizebyfour)
             if (resulthp > 0){
                 TheGame.Player.Health = resulthp
                 dmgsound.play()
@@ -290,11 +305,17 @@ function MeteorMove(){
         for (let j = 0; j < TheGame.Bullets.length; j++) {
             if (Distance(TheGame.Meteors[i].X, TheGame.Meteors[i].Y, TheGame.Bullets[j].X, TheGame.Bullets[j].Y) <= scaletopow)
             {
-                TheGame.Player.Score += Math.round(TheGame.Meteors[i].Size/4)
+                TheGame.Player.Score += Math.round(sizebyfour)
+                if (TheGame.Meteors[i].IsHealing){
+                    TheGame.Constants.HealGreen = 150
+                    TheGame.Player.Health = clamp(TheGame.Player.Health + Math.round(sizebyfour), 0, 100)  
+                    healsound.play()
+                }else{
+                    hitsound.play()
+                }
                 deleted = true
                 TheGame.Meteors.splice(i, 1)
                 TheGame.Bullets.splice(j, 1)
-                hitsound.play()
                 break;
             }
         }
@@ -317,6 +338,8 @@ function MeteorSpawner(){
         let XorY = getRandomInt(0,1)
         let PosX
         let PosY
+        let healing = false
+        let twox = false
         if (XorY){
             PosX = getRandomInt(0,1)
             if (PosX)
@@ -335,8 +358,14 @@ function MeteorSpawner(){
             }
             PosX = getRandomInt(0,w)
         }
-          
-        TheGame.SpawnMeteor(PosX,PosY,size, getRandomInt(1, 6))
+        if (TheGame.Player.Health < 100)
+        {
+            let rand = getRandomInt(1, 5)
+            if (rand == 5){
+                healing = true
+            }
+        }
+        TheGame.SpawnMeteor(PosX,PosY,size, getRandomInt(1, 5), healing, twox)
         lastspawned = CurTime()
     }
 }
@@ -357,7 +386,10 @@ function Tick(){
     {
         TheGame.Constants.HurtRed -= 2
     }
-
+    if (TheGame.Constants.HealGreen > 0)
+    {
+        TheGame.Constants.HealGreen -= 2
+    }
     if (CurTime() - TheGame.Player.NewTime <= (TheGame.Player.RotationTime))
     {
         let add = (TheGame.Player.NewRotation - TheGame.Player.OldRotation)
@@ -408,7 +440,7 @@ function DrawMeteors(){
         
         ctx.rotate(rot);
 
-        Line(ctx, 0, 0, 0, Scale(10), 1, "white")
+        //Line(ctx, 0, 0, 0, Scale(10), 1, "white")
         
         ctx.fillStyle = "gray"
         ctx.beginPath();
@@ -417,8 +449,24 @@ function DrawMeteors(){
         ctx.lineWidth = Scale(3);
         ctx.strokeStyle = '#4a4a4a';
         ctx.stroke();
-
         ctx.resetTransform();
+        
+        if (TheGame.Meteors[i].IsHealing)
+        {
+            ctx.save();
+            ctx.translate(TheGame.Meteors[i].X, TheGame.Meteors[i].Y);
+            ctx.textBaseline = "middle";
+            ctx.textAlign = "center"
+            ctx.font = "bold "+Scale(TheGame.Meteors[i].Size)+"px serif"
+            ctx.fillStyle = "rgba(255,100,100,1)"
+            ctx.fillText("+", 0,0);
+            ctx.lineWidth = Scale(1);
+            ctx.strokeStyle = "black"
+            ctx.strokeText("+",0,0)
+            ctx.restore();
+        }
+
+        
         
         ctx.fillStyle = "white"
     }
@@ -451,9 +499,9 @@ function draw(time){
    //let cos = (mousex - (w/2)) / Distance(w/2,h/2, mousex, mousey)
    //TheGame.Player.Rotation = Math.acos(cos)*180/Math.PI
    
-    ctx.font = Scale(32)+"px NafteraBoldItalic";
-    //ctx.fillText(TheGame.Player.Rotation, 10, Scale(60));
-    //ctx.fillText(TheGame.Player.NewRotation, 10, Scale(90));
+    //ctx.font = Scale(32)+"px NafteraBoldItalic";
+    //ctx.fillText(mousex, Scale(10), Scale(60));
+    //ctx.fillText(mousey, Scale(10), Scale(90));
     //ctx.fillText(TheGame.Player.OldRotation, 10, Scale(120));
     //ctx.fillText((TheGame.Player.NewRotation - TheGame.Player.OldRotation), 10, Scale(150));
     //angledLine(ctx, w/2, h/2, w*h, 1, TheGame.Player.Rotation, "white") 
@@ -476,6 +524,8 @@ function draw(time){
 
     ctx.fillStyle = "rgba(255,0,0,"+TheGame.Constants.HurtRed/255+")"
     ctx.fillRect(0, 0, w, h)
+    ctx.fillStyle = "rgba(0,255,0,"+TheGame.Constants.HealGreen/255+")"
+    ctx.fillRect(0, 0, w, h)
     if (!TheGame.Constants.GameOver){
         if (!TheGame.Constants.Started){
             ctx.save();
@@ -485,6 +535,7 @@ function draw(time){
             ctx.fillStyle = "white"
             ctx.font = Scale(40)+"px NafteraBoldItalic";
             ctx.textAlign = "center"
+            ctx.textBaseline = "middle";
             ctx.fillText("Press anywhere to start", 0, 0);
             ctx.restore()
         }else{
@@ -493,6 +544,7 @@ function draw(time){
             let rot = Math.sin(time/720) * Math.PI /50
             ctx.rotate(rot);
             ctx.textAlign = "center"
+            ctx.textBaseline = "middle";
             ctx.fillStyle = "rgba(100,255,100,1)"
             ctx.font = Scale(20)+"px NafteraBoldItalic";
             ctx.fillText("Score: "+TheGame.Player.Score, 0, 0);
@@ -508,6 +560,7 @@ function draw(time){
         ctx.fillStyle = "rgba(255,100,100,1)"
         ctx.font = Scale(40)+"px NafteraBoldItalic";
         ctx.textAlign = "center"
+        ctx.textBaseline = "middle";
         ctx.lineWidth = Scale(4);
         ctx.strokeStyle = "black"
         ctx.strokeText("You Died!", 0, 0);
